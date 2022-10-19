fc66ee7246aeca86e023ca5c5a8349bfffe6ce73
aCute5 <costantini95dario@gmail.com>
2022-10-19 15:46:18 +0200

    debugging

--- js/jscript.js
+++ js/jscript.js
@@ -4,4 +4,4 @@ let Surname = prompt("Qual è il tuo cognome?");
 document.getElementById("surname").innerHTML = Surname;
 let Color = prompt("Qual è il tuo colore preferito?");
 document.getElementById("color").innerHTML = Color;
-document.getElementById("allInfo").innerHTML = document.getElementById("name").innerHTML + document.getElementById("surname").innerHTML + document.getElementById("color").innerHTML + "22";
+document.getElementById("allInfo").innerHTML = Name + Surname + Color 
